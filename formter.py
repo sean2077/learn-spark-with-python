7b330dc981e0b4cacab53882c608c97041542ea1
zhangxianbing <zhangxianbing09@163.com>
2020-09-14 15:29:56 +0800

    feat: add rdd persist

--- formter.py
+++
@@ -1,9 +0,0 @@
-try:
-    sc.stop()
-except:
-    pass
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SparkSession
-
-sc = SparkContext()
-spark = SparkSession(sparkContext=sc)
